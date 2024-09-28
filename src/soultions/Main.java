93d5e3277dcb9675dce191dbf3e9256c48b1b451
Brandon-Knorr <bknorr@my.wctc.edu>
2024-09-28 18:20:54 -0500

    [1.00] Initial Commit - Sentence Smash

---
+++ src/soultions/Main.java
@@ -0,0 +1,17 @@
+package soultions;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    Sentence Smash
+    - Write a function that takes an array of words and smashes them together into a sentence and returns the sentence.
+    - Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+     */
+
+    public static String smash(String... words) {
+        return String.join(" ", words);
+    }
+}
