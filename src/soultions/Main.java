63282f9b18a4d67c139697419778afae0576d008
Brandon-Knorr <bknorr@my.wctc.edu>
2024-10-02 19:03:48 -0500

    Count by X

--- src/soultions/Main.java
+++ src/soultions/Main.java
@@ -39,4 +39,23 @@ public class Main {
     public static String numberToString(int num) {
         return String.format("%d", num);
     }
+
+
+
+    /*
+    Count by X
+    -Create a function with two arguments that will return an array of the first n multiples of x.
+    -Assume both the given number and the number of times to count will be positive numbers greater than 0.
+    -Return the results as an array or list.
+    */
+
+    public static int[] countBy(int x, int n){
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = x * (i + 1);
+        }
+        return result;
+    }
+
 }
